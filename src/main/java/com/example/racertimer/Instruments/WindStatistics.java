ebf83621c1ac1f34a8e5e3ea80fc4ac7a02176fe
unknown <assir@inbox.ru>
2022-03-06 08:14:51 +0700

    refactoried timer section for diagram comber. refactored diagram representative checking

--- src/main/java/com/example/racertimer/Instruments/WindStatistics.java
+++ src/main/java/com/example/racertimer/Instruments/WindStatistics.java
@@ -4,6 +4,9 @@ import android.location.Location;
 import android.os.CountDownTimer;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class WindStatistics { // класс для сбора статистики скоростей и рассчета истинного напр ветра
     private final static String PROJECT_LOG_TAG = "racer_timer_windStat";
 
@@ -19,8 +22,10 @@ public class WindStatistics { // класс для сбора статистик
     private int maxAllowedEmptyZone = 120; // наибольший разрешенный незаполненный сектор В ГРАДУСАХ
     private int maxCurrentEmptyZone; // максимальная текущая не заполненная зона В КОЛИЧЕСТВЕ СКТОРОВ
     // !!!при сравнении переменных не забыть о переводе количества секторо в градусы!!!
-    private int minFilledZones = 150; // минимальная разрешенная полностью заполненная зона диаграммы в градусах
-    private int summVelocity;
+    private int minFilledZones = 120; // минимальная разрешенная полностью заполненная зона диаграммы в градусах
+    // = 2 х 60 градусов
+    private int currentFilledSectors; // текущий счетчик количества заполненных секторов
+    private int summVelocity = 0;
 
     private int maxVelocity;
     // если есть хоть один незаполненный сектора больше этого, выборка считается не репрезентативной.
@@ -31,6 +36,8 @@ public class WindStatistics { // класс для сбора статистик
 
     WindChangedHerald windChangedHerald; // интерфейс для вывода нового значения ветра
     private CountDownTimer countDownTimer; // таймер для причесывания диаграммы когда не приходят новые данные
+    private Timer timer; // таймер для причесывания диаграммы когда какое-то время не приходят новые данные
+    private TimerTask timerTask; // тело задачи, которая выполняется по команде таймера
 
     public WindStatistics(int sizeOfSectors, WindChangedHerald windChangedHerald) {
         this.sizeOfSectors = sizeOfSectors;
@@ -44,20 +51,11 @@ public class WindStatistics { // класс для сбора статистик
             sin[i] = (float) Math.sin(Math.toRadians(currentAngle));
             cos[i] = (float) Math.cos(Math.toRadians(currentAngle));
 //            Log.i(PROJECT_LOG_TAG, " sin = " + sin[i] + ", cos = " + cos[i]); // проверено
+
         }
         this.windChangedHerald = windChangedHerald;
         sensitivity = 50; // параметр чувствистельности для настройки
 
-        countDownTimer = new CountDownTimer(60000, 1000) {
-            @Override
-            public void onTick(long l) {
-            }
-            @Override
-            public void onFinish() {
-                combTheDiagram(); // когда таймер закончился, причесываем диаграмму
-                Log.i(PROJECT_LOG_TAG, " combing the diagram... ");
-            }
-        };
     }
 
     public void setSensitivity(int updSensitivity) { // сеттер для возможности настройки на ходу
@@ -87,16 +85,35 @@ public class WindStatistics { // класс для сбора статистик
 
             // высчитываем новое направление ветра
             windDirection = calculateWindDirection();
-            countDownTimer.cancel();
-            countDownTimer.start();
+
+            if (windDiagramIsRepresentable) { //  запускаем/обнуляем таймер причесывания
+            }
 
             // проверяем условия и отправляем бродкаст
-            if (windDiagramIsRepresentable) // если репрезент. - отправляем бродкаст с новыми данными
-                if (lastWindDirection != windDirection) {
+            if (windDiagramIsRepresentable) {// если выборка репрезентативная, выполянем:
+
+                // обнуляем таймер
+                if (timerTask != null)  {
+                    timer.cancel(); // останавливаем ранее запущенный таймер
+                    timerTask.cancel();
+                }
+                timer = new Timer(); // пересоздаем таймер
+                timerTask = new TimerTask() { // создаем таймер таск, который будет выполнять таймер
+                    @Override
+                    public void run() {
+                        Log.i(PROJECT_LOG_TAG, " combing the diagram... ");
+                        combTheDiagram();
+                    }
+                };
+                timer.schedule(timerTask, 60000, 1000); // запускаем его заново
+
+                // отправляем бродкаст с новыми данными
+                if (lastWindDirection != windDirection) { // если зафиксировано изменение направления ветра с прошлой итерации
                     Log.i(PROJECT_LOG_TAG, "sending wind direction broadcast");
                     windChangedHerald.onWindDirectionChanged(windDirection);// отправляем broadcast с новым направлением
                 }
-            lastWindDirection = windDirection; // обновляем данные по направлению для фиксации изменений
+            }
+            lastWindDirection = windDirection; // обновляем данные по направлению ветра для фиксации изменений
         }
     }
 
@@ -129,11 +146,14 @@ public class WindStatistics { // класс для сбора статистик
         int windDirection = this.windDirection;
         double vectorLength;
         summVelocity = 0; // сумма длин всех векторов
+        currentFilledSectors = 0;
         double summX = 0; // координата X суммирующего вектора
         double summY = 0; // координата Y суммирующего вектора
 
         int representCounter = 0; // счетчик текущего не заполненного сектора
         maxCurrentEmptyZone = 0; // счетчик ширины максимального не заполненного сектора
+
+        // TODO: параметр maxVelocity походу не востребован в рамках текущей логики. Подумать - почистить
         maxVelocity = 0; // переменная для определения максимальной завфиксированной скорости
 
         for (int i = 0; i < numberOfSectors; i++) { // перебираем все сектора
@@ -142,8 +162,9 @@ public class WindStatistics { // класс для сбора статистик
                 if (windDiagram[i] == 0) representCounter ++;
                 else representCounter = 0;
                 if (representCounter > maxCurrentEmptyZone) maxCurrentEmptyZone = representCounter;
+                currentFilledSectors ++; // считаем количество заполненных секторов
             }
-            // находим координаты конца суммирующего вектора
+            // суммируем координаты текущего вектора -> находим координаты конца суммирующего вектора
             summX = summX + sin[i] * windDiagram[i]; // координата вектора X
             summY = summY + cos[i] * windDiagram[i]; // Координата вектора Y
 
@@ -165,17 +186,19 @@ public class WindStatistics { // класс для сбора статистик
 
     private boolean checkRepresentative() { // метод проверки выборки на репрезентативность
         boolean checkIsPassed = true;
-        // если ширина максимального незаполненного сектора больше указанной в настройках (перевод из секторов в градусы)
+        /** проверка по максимальной ширине незаполненного сектора
+         * если ширина максимального незаполненного сектора больше указанной в настройках (перевод из секторов в градусы)
+         */
         if (maxCurrentEmptyZone > maxAllowedEmptyZone / sizeOfSectors) checkIsPassed = false;
 
-        // если сумма скоростей всех секторов менее чем в X раз превышает скорость наибольшего вектора
-        // коэффициент требуемого превышения суммы по секторам над наибольшим сектором X = exсeedRatio
-        int exсeedRatio = minFilledZones / (sizeOfSectors * 4);
-        // среднюю скорость примем равную половине максимальной скорости
-        if (summVelocity < maxVelocity * exсeedRatio) checkIsPassed = false;
+        /**  проверка по количеству заполненных секторов */
+        // минимальное количество заполненных секторов
+        int minNumberOfFilledZones = minFilledZones / sizeOfSectors; // минимальное количество заполненных зон
+        if (currentFilledSectors < minNumberOfFilledZones) checkIsPassed = false;
 
         Log.i(PROJECT_LOG_TAG, "checking represent: "+checkIsPassed+", maxNotFilledSector = "+ maxCurrentEmptyZone
-                +", sumVelocities = "+ summVelocity +", maxVelocity*ratio = " +(maxVelocity * exсeedRatio));
+                +", min filled sectors = "+ minNumberOfFilledZones +", current filled = " + currentFilledSectors);
+
         if (checkIsPassed) Log.i(PROJECT_LOG_TAG, "max repres counter = "+ maxCurrentEmptyZone +", the wind diagram is being representative now!");
 
         return  checkIsPassed;
