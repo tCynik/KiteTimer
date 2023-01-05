cf09497319aa3b3aefff240faa1ece378adafe05
unknown <assir@inbox.ru>
2023-01-05 15:11:01 +0700

    added keeeping main activity screen on. code version setted to 2

--- src/main/java/com/tcynik/racertimer/main_activity/MainActivity.java
+++ src/main/java/com/tcynik/racertimer/main_activity/MainActivity.java
@@ -17,6 +17,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -119,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
         //todo: на устройстве пчмт навигационная панель (кнопки внизу) получается с белым фоном (в эмуляторе с черным). пытаюсь вообще убрать бар:
